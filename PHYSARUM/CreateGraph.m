ae42e25ab09c932db37a5eb02a9db8f2f6ba77b5
Aram Vroom <aram.vroom@strath.ac.uk>
2016-02-18 11:07:40 +0000

    St up CreateGraph function that generated the structure for the graph & update the CreateNode function add a node to this structure.

--- PHYSARUM/CreateGraph.m
+++ PHYSARUM/CreateGraph.m
@@ -11,16 +11,11 @@ function [Nodes] = CreateGraph()
 % CREATED BY:
 %   Aram Vroom - 2016
 
-
 Nodes = struct('identifier',      [1],... %
                'parent',          [0],... %Matrix containing each node's parent
                'links',           [0],... %Matrix that holds the nodes' connections to each other
                'costs',           [0],... %Matrix containing each connection's cost
                'probabilities',   [0] ... %Matrix containing the probability for each connection
                ); 
-
-    
-
-
-end
+ end
 
