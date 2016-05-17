9d3865398cbe1ecccfe5619e9682fc76f88ebc5e
Aram Vroom <aram.vroom@strath.ac.uk>
2016-05-17 16:43:47 +0100

    Added option for custom "BestChain" function & updated to SMART-O2C output

--- Optimisation/AIDMAP/CreateListNodes.m
+++ Optimisation/AIDMAP/CreateListNodes.m
@@ -11,11 +11,31 @@ function [ListNodes] = CreateListNodes(Inputs)
 % Email:  aram.vroom@strath.ac.uk
 
 
+%%%Create UID%%%
+%As only underscores can be used in field names, the UID is made up as follows:
+%3 underscores define the difference between the target and the
+%  attributes
+%2 underscores define the difference between two attributes
+%1 underscore denotes the location of the decimal point within an
+%  attribute
+
+rootattribstr = [];
+for i = 1:(length(Inputs.RootAttrib)*2-1)
+    if rem(i,2)
+        rootattribstr = strcat(rootattribstr,num2str(Inputs.RootAttrib((i+1)/2)));
+    else
+        rootattribstr = strcat(rootattribstr,'__');
+    end
+end
+
+rootattribstr = strrep(rootattribstr,'.','_');
+
+rootID = strcat(Inputs.RootName,'___',rootattribstr);
 
 %Create the ListNodes structure
-ListNodes = struct(struct(Inputs.RootName,   ...
+ListNodes = struct(struct(rootID,   ...
                          struct(...
-                         'node_ID',           Inputs.RootName,...
+                         'node_ID',           rootID,...
                          'parent',            [],... % The parent of the node
                          'children',          [],... % Matrix that holds the nodes' connections to each other
                          'radius',            [],... % The radius of each connection
---
+++ Problems/AIDMAP/Atira/MyBestChain.m
@@ -0,0 +1,35 @@
+function [bestchainindex, bestcost] = MyBestChain(numberofnodes,nodecosts)
+% This function fnd the best chain to be used in the growth factor
+% functionality
+
+% Inputs:
+% * numberofnodes : Vector with the amount of nodes for each solution so
+%                   far
+% * nodecosts     : Vector with the costs for each solution
+%
+% Outputs: 
+% * bestchainindex  : The index of the best chain within the numberofnodes
+%                      nodecosts vectors
+% * bestcost        : The cost of the best solution
+%
+% Author: Aram Vroom - 2016
+% Email:  aram.vroom@strath.ac.uk
+
+%Find the index of the solution(s) with the most asteroids
+maxasteroidnumindex = find(numberofnodes==max(numberofnodes));
+
+%Find the corresponding cost(s)
+maxasteroidcosts = nodecosts(maxasteroidnumindex);
+
+%Obtain the index of the minimum cost
+minimumcostindex = find(maxasteroidcosts==min(maxasteroidcosts));
+
+%Find the index of the solution with the moster asteroids and the minimum
+%cost
+bestchainindex = maxasteroidnumindex(minimumcostindex);
+
+%Retreive the best cost
+bestcost = maxasteroidcosts(minimumcostindex);
+
+end
+
