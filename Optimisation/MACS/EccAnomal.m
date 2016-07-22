ad5893ee93fb266247114b66ccdd4454cd8d1d72
annalisa <nina1983@gmail.com>
2016-07-22 17:39:25 +0100

    cleanind MACS

--- Optimisation/MACS/EccAnomal.m
+++
@@ -1,3 +0,0 @@
-function Ec = EccAnomal(ni,ecc)
-
-Ec = 2*atan2(sqrt((1-ecc)/(1+ecc))*sin(ni/2),cos(ni/2));
--- Optimisation/MACS/smooth_cheb_fun.m
+++
@@ -1,7 +0,0 @@
-function [f,df]=smooth_cheb_fun(x)
-
-f = x(1);
-df = 0*x;
-df(1) = 1;
-
-return
