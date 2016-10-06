ab90b7c35a1a0eceb91e86a5acfe416319f7205b
carlos <carlos.ortega@strath.ac.uk>
2016-10-06 20:19:47 +0100

    starting mo

--- Temporary/MACSMINMAX_CURRENT/ALGORITHM/META/MACSMINMAX/METAPROBLEM_INNER/mask_objfun_ideaminmax_s_inner.m
+++ Temporary/MACSMINMAX_CURRENT/ALGORITHM/META/MACSMINMAX/METAPROBLEM_INNER/mask_objfun_ideaminmax_s_inner.m
@@ -1,5 +1,7 @@
 function [masked] = mask_objfun_ideaminmax_s_inner(u,par_objfun)
 
-masked = -par_objfun.surrogate.indicator([par_objfun.d u],par_objfun.ymin,par_objfun.surrogate); % negative because we will always maximize PI or EI
+indicators = -par_objfun.surrogate.indicator([par_objfun.d u],par_objfun.ymin,par_objfun.surrogate); % negative because we will always maximize PI or EI
+
+masked = indicators(par_objfun.objective);
 
 return
